f9244bdd67ff6710f529333c7d51530f7e99825b
Adrien Poupard <adrien.poupard@gmail.com>
2015-08-19 22:08:50 +1200

    move js in sesparate file

---
+++ src/main/webapp/lib/topophone.js
@@ -0,0 +1,22 @@
+function startCarousel() {
+	if($('.jcarousel').size()>0){
+		$('.jcarousel').jcarousel({
+			wrap : 'circular'
+		}).jcarouselAutoscroll({
+			interval : 3000,
+			target : '+=1',
+			autostart : true
+		});
+	}
+}
+function startSoundPlayer() {
+	if($('.sm2-bar-ui').size()>0){
+		window.SM2BarPlayer($('.sm2-bar-ui')[0])
+	}
+}
+function startShowVideo(url) {
+	if ($('.youtubePopup').attr('data') != url) {
+		$('.youtubePopup').attr('data', url);
+	}
+	$('.toHide.hidden').removeClass('hidden');
+}
