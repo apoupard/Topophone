5305f435250508c8584d7b6c568466b556783147
Adrien Poupard <adrien.poupard@gmail.com>
2015-09-25 19:56:48 +1200

    Main page improvement

--- src/main/java/io/enscene/topophone/dao/MainDao.java
+++ src/main/java/io/enscene/topophone/dao/MainDao.java
@@ -13,7 +13,7 @@ public class MainDao extends AstractHardCoded<Main> {
 
   @Override
   void init(Map<String, Main> database) {
-    database.put("1", new Main(new HeadbandCarousel(getEducationHeadbandImages(), "/images/education/headbandlogo.png"), getEnginedTemplate("main", "description"), new Youtube("l3kcrbm2aNc"), new Youtube("31o3xo3Lohg")));
+    database.put("1", new Main(new HeadbandCarousel(getEducationHeadbandImages(), "/images/education/headbandlogo.png"), getEnginedTemplate("main", "description"), new Youtube("l3kcrbm2aNc"), new Youtube("D1QOwfWPUZI")));
   }
   
   
