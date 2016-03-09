9c0c209844b80541f131f58b286d1da650115cbb
Adrien Poupard <adrien.poupard@gmail.com>
2016-03-09 22:25:34 +1100

    Use google autovalue bis

--- src/main/java/io/enscene/topophone/TopophoneModule.java
+++ src/main/java/io/enscene/topophone/TopophoneModule.java
@@ -4,24 +4,10 @@ import com.github.ziplet.filter.compression.CompressingFilter;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
 import com.google.inject.servlet.ServletModule;
-
 import freemarker.template.Configuration;
 import io.enscene.topophone.api.ResourceDao;
 import io.enscene.topophone.api.ResourceModel;
-import io.enscene.topophone.dao.AccompanyingDao;
-import io.enscene.topophone.dao.ArtistDao;
-import io.enscene.topophone.dao.ContactsDao;
-import io.enscene.topophone.dao.DevArtistsDao;
-import io.enscene.topophone.dao.EducationDao;
-import io.enscene.topophone.dao.FooterDao;
-import io.enscene.topophone.dao.HeaderDao;
-import io.enscene.topophone.dao.MainDao;
-import io.enscene.topophone.dao.NavDao;
-import io.enscene.topophone.dao.PartnersDao;
-import io.enscene.topophone.resource.AccompanyingResource;
-import io.enscene.topophone.resource.ArtistResource;
-import io.enscene.topophone.resource.EducationResource;
-import io.enscene.topophone.resource.PartnerResource;
+import io.enscene.topophone.dao.*;
 import io.enscene.topophone.templating.FreemakerTemplateEngine;
 import io.enscene.topophone.templating.HtmlTemplateEngine;
 import io.enscene.topophone.templating.resource.HeaderResource;
@@ -40,13 +26,9 @@ public class TopophoneModule extends ServletModule {
     bind(HtmlTemplateEngine.class).to(FreemakerTemplateEngine.class);
     bind(Configuration.class).toProvider(FreemarkerConfigurationProvider.class);
 
-    bind(ArtistResource.class);
     bind(NavResource.class);
     bind(HeaderResource.class);
-    bind(PartnerResource.class);
-    bind(EducationResource.class);
     bind(SectionsResource.class);
-    bind(AccompanyingResource.class);
     bind(IndexResource.class);
     
     MapBinder<String, ResourceDao<? extends ResourceModel>> resourceDaoBinder =
